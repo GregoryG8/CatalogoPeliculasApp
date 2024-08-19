63c1ba13071b5edc82f676a86373c778e984acf3
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-08-18 20:25:55 -0500

    feat: create conditional for not found movies

--- src/servicio/ServicioPeliculasLista.java
+++ src/servicio/ServicioPeliculasLista.java
@@ -29,6 +29,22 @@ public class ServicioPeliculasLista implements IServicioPeliculas {
     public void buscarPelicula(Pelicula pelicula) {
         //Regresa indice de la pelicula encontrada en la lista
         var indice = peliculas.indexOf(pelicula);
+        if (indice == -1) {
+            System.out.println("No se encontro la pelicula: " + pelicula);
+        }
         System.out.println("Pelicula encontrada en el indice: " + indice);
+    }
+
+    public static void main(String[] args) {
+        var pelicula1 = new Pelicula("Batman");
+
+        //Creamos el servicio (patron de diseno service)
+        IServicioPeliculas servicioPeliculas = new ServicioPeliculasLista();
+        //Agrega la pelicula a la lista
+        servicioPeliculas.agregarPelicula(pelicula1);
+        // Listar peliculas
+        servicioPeliculas.listarPeliculas();
+        //Buscar pelicula, se implementa equals y hash code
+        servicioPeliculas.buscarPelicula(pelicula1);
     }
 }
